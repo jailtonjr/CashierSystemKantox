a0bfeb5869e120577904da81e963794465514db2
jailton louzada <jailtonalkimin@gmail.com>
2023-05-20 20:45:32 +0200

    Initial commit 🔜🇪🇸

---
+++ src/main/java/Product.java
@@ -0,0 +1,40 @@
+/**
+ * Project: CashierSystemKantox
+ * Created by: Jailton Louzada
+ * WebSite: https://jailtonjr.dev
+ * Description: Product
+ */
+public class Product {
+
+    private String code;
+    private String name;
+    private Double price;
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public String printAll() {
+        return "Code:" +code+" Name:"+name+" Price:"+ price ;
+    }
+}
---
+++ src/main/java/Util.java
@@ -0,0 +1,51 @@
+import org.yaml.snakeyaml.Yaml;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+/**
+ * Project: CashierSystemKantox
+ * Created by: Jailton Louzada
+ * WebSite: https://jailtonjr.dev
+ * Description: Utils
+ */
+public final class Util {
+
+    public static Iterable<Object> loadFromYamlFile(String path) {
+        Yaml yaml = new Yaml();
+        InputStream in = Util.class.getResourceAsStream(path);
+        Iterable<Object> list = yaml.loadAll(in);
+
+        return list;
+    }
+
+    public static String productPath() {
+        Properties prop = new Properties();
+        String fileName = "src/main/resources/config.properties";
+        try (FileInputStream fis = new FileInputStream(fileName)) {
+            prop.load(fis);
+        } catch (FileNotFoundException ex) {
+
+        } catch (IOException ex) {
+
+        }
+        return prop.getProperty("products_path");
+    }
+
+    public static String rulesPath() {
+        Properties prop = new Properties();
+        String fileName = "src/main/resources/config.properties";
+        try (FileInputStream fis = new FileInputStream(fileName)) {
+            prop.load(fis);
+        } catch (FileNotFoundException ex) {
+
+        } catch (IOException ex) {
+
+        }
+        return prop.getProperty("rules_path");
+    }
+
+}
